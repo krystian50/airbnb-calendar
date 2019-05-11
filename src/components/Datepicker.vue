a2113f380faa0b8adb25a5aa014865d07b23c2d8
Krystian Fras <krystian@fras.me>
2019-05-11 15:12:43 +0200

    feat(components): added basic datepicker layout

--- src/components/Datepicker.vue
+++ src/components/Datepicker.vue
@@ -1,5 +1,44 @@
 <template>
   <div class="datepicker">
-    <input class="datepicker__input" type="text" readonly />
+    <input
+      class="datepicker__input"
+      type="text"
+      readonly
+      @click="toggleCalendar()"
+    />
+    <div v-if="isOpen" class="datepicker__calendar">
+      <datepicker-header
+        @go-back="changeMonthBy(-1)"
+        @go-next="changeMonthBy(1)"
+      />
+    </div>
   </div>
 </template>
+<script>
+import DatepickerHeader from "./DatepickerHeader";
+
+export default {
+  name: "Datepicker",
+  components: {
+    DatepickerHeader
+  },
+  props: {
+    value: {
+      type: Date,
+      required: false,
+      default: null
+    }
+  },
+  data: () => ({
+    isOpen: false
+  }),
+  methods: {
+    toggleCalendar() {
+      this.isOpen = !this.isOpen;
+    },
+    changeMonthBy(months) {
+      console.log(months);
+    }
+  }
+};
+</script>
---
+++ src/components/DatepickerHeader.vue
@@ -0,0 +1,13 @@
+<template functional>
+  <div class="datepicker-header">
+    <span class="datepicker-header__arrow" @click="listeners['go-back']()">
+      &larr;
+    </span>
+    <span class="datepicker-header__header">
+      may 2017
+    </span>
+    <span class="datepicker-header__arrow" @click="listeners['go-next']()">
+      &rarr;
+    </span>
+  </div>
+</template>
