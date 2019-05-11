b331b6c9233accc289af0591fda304f36bf4bfcc
Krystian Fras <krystian@fras.me>
2019-05-11 15:45:46 +0200

    refactor(helpers): added docs

--- src/helpers/number.js
+++ src/helpers/number.js
@@ -1,2 +1,14 @@
+/**
+ * Checks if value is between range
+ * @param {number} min - minimal value
+ * @param {number} max - maximal value
+ * @returns {function} a function that validate if value is between
+ */
 export const isBetween = (min, max) => val => min <= val && val <= max;
+
+/**
+ * Checks if value is positive
+ * @param {number} val - given value
+ * @returns {bolean}
+ */
 export const isPositive = val => 0 <= val;
