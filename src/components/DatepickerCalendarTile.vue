d9412ec596dda437487e763e8c0e6b96866a8893
Krystian Fras <krystian@fras.me>
2019-05-12 14:54:35 +0200

    feat(components): added datepicker calendar tile  basic styles

--- src/components/DatepickerCalendarTile.vue
+++ src/components/DatepickerCalendarTile.vue
@@ -1,3 +1,9 @@
 <template functional>
-  <td>{{ props.dayInMonth }}</td>
+  <td class="datepicker-calendar-tile">{{ props.dayInMonth }}</td>
 </template>
+<style lang="scss" scoped>
+.datepicker-calendar-tile {
+  border: 1px solid $border-dark-color;
+  padding: 3px;
+}
+</style>
